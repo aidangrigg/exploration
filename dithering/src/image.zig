bf71bf84c0564a62e33cacfddd0f9a0f3f9e66f9
Aidan Grigg <aidangrigg02@gmail.com>
2025-07-26 16:30:38 +1000

    dithering: Refactored, added CLI arguments / basic handling

--- dithering/src/image.zig
+++ dithering/src/image.zig
@@ -4,33 +4,35 @@ const stbi = @cImport(@cInclude("stb_image.h"));
 const stbiw = @cImport(@cInclude("stb_image_write.h"));
 
 pub const Pixel = struct {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+    rgb: [3]f32,
 
-    pub fn fromSlice(slice: []u8) !Pixel {
-        switch (slice.len) {
-            4 => {
-                return .{
-                    .r = slice[0],
-                    .g = slice[1],
-                    .b = slice[2],
-                    .a = slice[3],
-                };
-            },
-            3 => {
-                return .{
-                    .r = slice[0],
-                    .g = slice[1],
-                    .b = slice[2],
-                    .a = 1,
-                };
-            },
-            else => {
-                return error.Bounds;
-            },
+    pub fn init(data: []u8) Pixel {
+        std.debug.assert(data.len != 0 and data.len < 4);
+
+        var px: Pixel = undefined;
+
+        for (0..3) |i| {
+            const idx = if (data.len <= 2) 0 else i;
+            px.rgb[i] = @floatFromInt(data[idx]);
         }
+
+        return px;
+    }
+
+    pub fn r(self: Pixel) f32 {
+        return self.rgb[0];
+    }
+
+    pub fn g(self: Pixel) f32 {
+        return self.rgb[1];
+    }
+
+    pub fn b(self: Pixel) f32 {
+        return self.rgb[2];
+    }
+
+    pub fn luminosity(self: Pixel) f32 {
+        return std.math.clamp(0.299 * self.r() + 0.587 * self.g() + 0.114 * self.b(), 0, 255);
     }
 };
 
@@ -39,10 +41,8 @@ pub const Image = struct {
     height: usize,
     channels: u8,
     data: std.ArrayList(Pixel),
-    c_data: []u8,
 
-    pub fn free(self: Image) void {
-        stbi.stbi_image_free(self.c_data.ptr);
+    pub fn deinit(self: Image) void {
         self.data.deinit();
     }
 
@@ -52,34 +52,88 @@ pub const Image = struct {
         var channels: c_int = undefined;
 
         var c_data = stbi.stbi_load(path, &width, &height, &channels, 0);
+        defer stbi.stbi_image_free(c_data);
 
         var img: Image = undefined;
         img.width = @intCast(width);
         img.height = @intCast(height);
         img.channels = @intCast(channels);
-        img.data = std.ArrayList(Pixel).init(alloc);
-
-        img.c_data = c_data[0 .. img.height * img.width * img.channels];
+        img.data = try std.ArrayList(Pixel).initCapacity(alloc, img.width * img.height);
 
         var i: usize = 0;
-        while (i < img.c_data.len) : (i += img.channels) {
-            const slice = img.c_data[i .. i + img.channels];
-            const pixel = try Pixel.fromSlice(slice);
-            try img.data.append(pixel);
+        while (i < img.width * img.height) : (i += 1) {
+            const j = i * img.channels;
+            const slice = c_data[j .. j + img.channels];
+            img.data.appendAssumeCapacity(Pixel.init(slice));
         }
 
         return img;
     }
 
-    pub fn write(self: Image, alloc: std.mem.Allocator) !void {
+    pub fn find_closest_palette_color(px: Pixel) Pixel {
+        if (px.luminosity() > 127.0) {
+            return .{
+                .rgb = .{ 255, 255, 255 },
+            };
+        } else {
+            return .{
+                .rgb = .{ 0, 0, 0 },
+            };
+        }
+    }
+
+    fn apply_quant(px: *Pixel, quant_r: f32, quant_g: f32, quant_b: f32, factor: f32, div: f32) void {
+        px.rgb[0] += quant_r * factor / div;
+        px.rgb[1] += quant_g * factor / div;
+        px.rgb[2] += quant_b * factor / div;
+    }
+
+    pub fn dither(self: *Image) void {
+        for (0..self.height) |y| {
+            for (0..self.width) |x| {
+                const idx = y * self.width + x;
+                const old_pixel = self.data.items[idx];
+                const new_pixel = find_closest_palette_color(old_pixel);
+                self.data.items[idx] = new_pixel;
+                const quant_r = old_pixel.r() - new_pixel.r();
+                const quant_g = old_pixel.g() - new_pixel.g();
+                const quant_b = old_pixel.b() - new_pixel.b();
+
+                if (x + 1 < self.width) {
+                    const val = &self.data.items[idx + 1];
+                    apply_quant(val, quant_r, quant_g, quant_b, 7, 16);
+                }
+
+                if (x >= 1 and y + 1 < self.height) {
+                    const val = &self.data.items[idx - 1 + self.width];
+                    apply_quant(val, quant_r, quant_g, quant_b, 3, 16);
+                }
+
+                if (y + 1 < self.height) {
+                    const val = &self.data.items[idx + self.width];
+                    apply_quant(val, quant_r, quant_g, quant_b, 5, 16);
+                }
+
+                if (x + 1 < self.width and y + 1 < self.height) {
+                    const val = &self.data.items[idx + 1 + self.width];
+                    apply_quant(val, quant_r, quant_g, quant_b, 1, 16);
+                }
+            }
+        }
+    }
+
+    fn clamptoU8(val: f32) u8 {
+        return @intFromFloat(std.math.clamp(val, 0, 255));
+    }
+
+    pub fn write(self: Image, path: [:0]const u8, alloc: std.mem.Allocator) !void {
         var data = std.ArrayList(u8).init(alloc);
-        for (self.data.items) |pixel| {
-            try data.append(if (pixel.r == 1.0) 255 else 0);
+        for (self.data.items) |px| {
+            try data.append(clamptoU8(px.r()));
+            try data.append(clamptoU8(px.g()));
+            try data.append(clamptoU8(px.b()));
         }
 
-        std.debug.print("data size: {}\n", .{data.items.len});
-        std.debug.print("expected size: {}\n", .{self.height * self.width});
-
-        _ = stbiw.stbi_write_bmp("./out.bmp", @intCast(self.width), @intCast(self.height), 1, data.items.ptr);
+        _ = stbiw.stbi_write_png(path, @intCast(self.width), @intCast(self.height), 3, data.items.ptr, @intCast(self.width * 3));
     }
 };
--- dithering/src/main.zig
+++ dithering/src/main.zig
@@ -1,29 +1,67 @@
 const std = @import("std");
 
 const img = @import("image.zig");
-const stbiw = @cImport(@cInclude("stb_image_write.h"));
 
-fn greyscale(px: img.Pixel) u8 {
-    const r: f32 = @floatFromInt(px.r);
-    const g: f32 = @floatFromInt(px.g);
-    const b: f32 = @floatFromInt(px.b);
-    const grey = std.math.clamp(0.299 * r + 0.587 * g + 0.114 * b, 0, 255);
-    return @intFromFloat(grey);
-}
+const Args = struct {
+    input_path: [:0]const u8 = "",
+    output_path: [:0]const u8 = "out.png",
+    help: bool = false,
+};
 
-fn clampToU8(val: f32) u8 {
-    return @intFromFloat(@min(@max(val, 0.0), 255.0));
-}
+const CommandLineErrors = error{
+    MissingInputFile,
+    MissingArg,
+};
 
-pub fn main() !void {
+fn processArgs() !Args {
     var args = std.process.args();
     _ = args.skip();
 
-    const filename = args.next();
+    var out = Args {};
+    var input_path_set = false;
 
-    if (filename == null) {
-        std.debug.print("ERROR: Filename must be passed in", .{});
-        return error.MissingFilename;
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "-o") or std.mem.eql(u8, arg, "--output")) {
+            if (args.next()) |output_path| {
+                out.output_path = output_path;
+            } else {
+                return CommandLineErrors.MissingArg;
+            }
+        } else if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
+            out.help = true;
+        } else {
+            input_path_set = true;
+            out.input_path = arg;
+        }
+    }
+
+    if (input_path_set) {
+        return out;
+    } else {
+        return CommandLineErrors.MissingInputFile;
+    }
+}
+
+pub fn main() !void {
+    const out = std.io.getStdOut().writer();
+    const args = processArgs() catch |err| {
+        if (err == CommandLineErrors.MissingInputFile) {
+            try out.print("Must be called with a input file.\n", .{});
+        } else if (err == CommandLineErrors.MissingArg) {
+            try out.print("Missing args, consult '--help' for help.\n", .{});
+        }
+        std.process.exit(1);
+    };
+
+    if (args.help) {
+        try out.print(
+                \\Usage: dither [OPTION]... [INPUT]
+                \\Applies Floyd–Steinberg dithering to an input image and outputs a PNG.
+                \\   -o, --output        The file to output to.
+                \\   -h, --help          Show this help menu\n
+                , .{}
+        );
+        std.process.exit(0);
     }
 
     const gpa = std.heap.page_allocator;
@@ -32,50 +70,9 @@ pub fn main() !void {
 
     const allocator = arena.allocator();
 
-    var image = try img.Image.load(filename.?, allocator);
-    defer image.free();
+    var image = try img.Image.load(args.input_path, allocator);
+    defer image.deinit();
 
-    std.debug.print("Width: {}, Height: {}, Channels: {}", .{ image.width, image.height, image.channels });
-
-    var data = std.ArrayList(u8).init(allocator);
-
-    for (image.data.items) |pixel| {
-        try data.append(greyscale(pixel));
-    }
-
-    for (0..image.height) |y| {
-        for (0..image.width) |x| {
-            const idx = y * image.width + x;
-            const old_pixel: f32 = @floatFromInt(data.items[idx]);
-            const new_pixel: f32 = if (old_pixel > 127) 255.0 else 0.0;
-            data.items[idx] = clampToU8(new_pixel);
-            const quant_error = old_pixel - new_pixel;
-
-            if (x + 1 < image.width) {
-                const i = idx + 1;
-                const val: f32 = @floatFromInt(data.items[i]);
-                data.items[i] = clampToU8(val + quant_error * 7.0 / 16.0);
-            }
-
-            if (x >= 1 and y + 1 < image.height) {
-                const i = idx - 1 + image.width;
-                const val: f32 = @floatFromInt(data.items[i]);
-                data.items[i] = clampToU8(val + quant_error * 3.0 / 16.0);
-            }
-
-            if (y + 1 < image.height) {
-                const i = idx + image.width;
-                const val: f32 = @floatFromInt(data.items[i]);
-                data.items[i] = clampToU8(val + quant_error * 5.0 / 16.0);
-            }
-
-            if (x + 1 < image.width and y + 1 < image.height) {
-                const i = idx + 1 + image.width;
-                const val: f32 = @floatFromInt(data.items[i]);
-                data.items[i] = clampToU8(val + quant_error * 1.0 / 16.0);
-            }
-        }
-    }
-
-    _ = stbiw.stbi_write_png("./out.png", @intCast(image.width), @intCast(image.height), 1, data.items.ptr, @intCast(image.width));
+    image.dither();
+    try image.write(args.output_path, allocator);
 }
